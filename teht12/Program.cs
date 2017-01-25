e3025381d5d58f835e0676811c4853c2fc45136d
k2751 <k2751@Student.jamk.fi>
2017-01-25 14:20:40 +0200

    TEHT12 finished

--- teht12/Program.cs
+++ teht12/Program.cs
@@ -13,16 +13,19 @@ namespace teht12
         
             int x;
             int[] lista = new int[5];
+            Console.WriteLine("Insert five numbers to reverse their order");
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Give a number  > ");
                 x = int.Parse(Console.ReadLine());
                 lista[i] = x;
             }
-            for (int i = 4; i > 0; i--)
+            Console.Write(lista[4]);
+            for (int i = 3; i >= 0; i--)
             {
-                Console.Write(lista[i] + ", ");
+                Console.Write(", " + lista[i]);
             }
+            Console.WriteLine();
            
                     
                  
